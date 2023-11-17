d4fc72daac7b0f1f6054a3425166c65e53c28658
Felipe <felipejaber.dev@gmail.com>
2023-11-17 14:17:16 -0300

    startProject

---
+++ src/main/java/com/pi4p/BackendPi4pApplication.java
@@ -0,0 +1,13 @@
+package com.pi4p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPi4pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPi4pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi4p/BackendPi4pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi4p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPi4pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
